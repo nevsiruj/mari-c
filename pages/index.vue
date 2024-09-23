8a490b5151d77e151b99e7b4f609cf6b7d92b414
nevsiruj <ngonzalosuarez@gmail.com>
2024-09-22 22:03:47 -0300

    go

--- pages/index.vue
+++ pages/index.vue
@@ -2,23 +2,17 @@
 
   <div class="font-sans antialiased">
     <!-- Hero Section -->
-    <section
-  id="home"
-  class="bg-cover bg-center h-screen flex items-center justify-center text-center text-white"
-  style="background-image: url('https://source.unsplash.com/1600x900/?meditation')"
->
-  <div class="bg-gradient-to-r from-pink-300 via-pink-400 to-pink-500 bg-opacity-70 p-12 rounded">
-    <h2 class="text-4xl font-bold mb-4">Curso de Terapias Holísticas</h2>
-    <p class="text-lg mb-8">
-      Únete a mi curso de Armonización de Chakras, Reiki Angelical y Registros Akashicos. Transforma tu vida y acompaña a otras personas.
-    </p>
-    <a
-      href="#course"
-      class="bg-purple-400 text-white py-2 px-6 rounded hover:bg-purple-300"
-      >Descubre Más</a
-    >
-    <!-- Agregar el video aquí -->
-    <!-- <div class="mt-8">
+    <section id="home" class="bg-cover bg-center h-screen flex items-center justify-center text-center text-white"
+      style="background-image: url('https://source.unsplash.com/1600x900/?meditation')">
+      <div class="bg-gradient-to-r from-pink-300 via-pink-400 to-pink-500 bg-opacity-70 p-12 rounded">
+        <h2 class="text-4xl font-bold mb-4">Curso de Terapias Holísticas</h2>
+        <p class="text-lg mb-8">
+          Únete a mi curso de Armonización de Chakras, Reiki Angelical y Registros Akashicos. Transforma tu vida y
+          acompaña a otras personas.
+        </p>
+        <a href="#course" class="bg-purple-400 text-white py-2 px-6 rounded hover:bg-purple-300">Descubre Más</a>
+        <!-- Agregar el video aquí -->
+        <!-- <div class="mt-8">
       <iframe 
         src="https://drive.google.com/file/d/16JzALek7DM-gLTtaAmBljNLvdETI-tsN/preview" 
         width="560" 
@@ -29,26 +23,21 @@
       </iframe>
     </div> -->
 
-  </div>
-</section>
+      </div>
+    </section>
 
 
 
-     <!-- Video Section -->
+    <!-- Video Section -->
     <section id="video" class="py-12 bg-purple-100">
       <div class="container mx-auto text-center">
         <h2 class="text-3xl font-bold mb-4 text-purple-800">Mira Nuestro Video</h2>
         <div class="flex justify-center">
-          <iframe
-            width="560"
-            height="315"
-            src="https://drive.google.com/file/d/16JzALek7DM-gLTtaAmBljNLvdETI-tsN/preview"
-            title="Video del Curso"
+          <iframe width="560" height="315"
+            src="https://drive.google.com/file/d/16JzALek7DM-gLTtaAmBljNLvdETI-tsN/preview" title="Video del Curso"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen
-            class="rounded"
-          ></iframe>
+            allowfullscreen class="rounded"></iframe>
         </div>
       </div>
     </section>
@@ -58,8 +47,9 @@
       <div class="container mx-auto text-center">
         <h2 class="text-3xl font-bold mb-4 text-purple-800">Sobre Mí</h2>
         <p class="text-lg text-purple-700">
-          Soy Mariel Cuevas, apasionada Profesora de Yoga, Terapeuta en Flores de Bach, Consteladora Familiar, 
-          Master Reiki Usui y Angelical, Lectora de Registro Akashicos y Coach Ontológica. Mi misión es acompañarte en procesos de transformación para lograr tu equilibrio
+          Soy Mariel Cuevas, apasionada Profesora de Yoga, Terapeuta en Flores de Bach, Consteladora Familiar,
+          Master Reiki Usui y Angelical, Lectora de Registro Akashicos y Coach Ontológica. Mi misión es acompañarte en
+          procesos de transformación para lograr tu equilibrio
           y bienestar por medio de diferentes técnicas y terapias.
         </p>
       </div>
@@ -69,39 +59,45 @@
     <section id="course" class="py-12 bg-purple-100">
       <div class="container mx-auto text-center">
         <h2 class="text-3xl font-bold mb-4 text-purple-800">Curso Especial</h2>
-                  <h3 class="text-xl font-bold mb-2 text-purple-800">Armonización de Chakras, Reiki Angelical y Registros Akashicos</h3>
+        <h3 class="text-xl font-bold mb-2 text-purple-800">Armonización de Chakras, Reiki Angelical y Registros
+          Akashicos</h3>
 
-     
+
         <h3 class="text-xl font-bold mt-8 mb-4 text-purple-800">Temario del Curso</h3>
         <div class="bg-white p-6 rounded shadow-lg mb-6">
           <h4 class="text-lg font-bold mb-2 text-purple-800">Clase 1: Armonización de Chakras</h4>
-          <p class="text-gray-700">Qué son, la importancia de la armonización de los chakras en nuestra vida. Diferentes técnicas de armonización. Incluye audio de meditación para armonizar.</p>
-          <iframe 
-  src="https://drive.google.com/file/d/1-H_QaEYgrVLFv61oVfbTauxljrUJnTnf/preview" 
-  width="800" 
-  height="450" 
-  class="w-full h-auto object-cover rounded mt-4" 
-  allow="autoplay">
-</iframe>
-     </div>
+          <p class="text-gray-700">Qué son, la importancia de la armonización de los chakras en nuestra vida. Diferentes
+            técnicas de armonización. Incluye audio de meditación para armonizar.</p>
+          <iframe src="https://drive.google.com/file/d/1-H_QaEYgrVLFv61oVfbTauxljrUJnTnf/preview" width="800"
+            height="450" class="w-full h-auto object-cover rounded mt-4" allow="autoplay">
+          </iframe>
+        </div>
         <div class="bg-white p-6 rounded shadow-lg mb-6">
           <h4 class="text-lg font-bold mb-2 text-purple-800">Clase 2: Reiki Angelical</h4>
-          <p class="text-gray-700">Sintonización en Reiki Angelical y Maestría. Este curso te permitirá dar Reiki y sintonizar a otras personas.</p>
+          <p class="text-gray-700">Sintonización en Reiki Angelical y Maestría. Este curso te permitirá dar Reiki y
+            sintonizar a otras personas.</p>
           <!-- <img src="https://drive.google.com/file/d/1aGnFxn64OWv1a8ZoUeZFv0UswNSeT4a6/preview" alt="Clase de Reiki Angelical" class="w-full h-40 object-cover rounded mt-4" />
         
           <iframe src="https://drive.google.com/file/d/1aGnFxn64OWv1a8ZoUeZFv0UswNSeT4a6/preview" width="800" height="600" allow="autoplay"></iframe> -->
-          <iframe src="https://drive.google.com/file/d/1aGnFxn64OWv1a8ZoUeZFv0UswNSeT4a6/preview" width="800" height="600" class="w-full h-40 object-cover rounded mt-4" allow="autoplay"></iframe>
+          <iframe src="https://drive.google.com/file/d/1aGnFxn64OWv1a8ZoUeZFv0UswNSeT4a6/preview" width="800"
+            height="600" class="w-full h-40 object-cover rounded mt-4" allow="autoplay"></iframe>
 
         </div>
         <div class="bg-white p-6 rounded shadow-lg">
           <h4 class="text-lg font-bold mb-2 text-purple-800">Clases 3 y 4: Registros Akashicos</h4>
-          <p class="text-gray-700">Nivel I: Lectura de Registros Akashicos, qué es el Akasha, cómo abrir nuestros propios registros, práctica en vivo.<br>
-          Nivel II y III: Maestría en Lectura de Registros Akashicos, cómo abrir registros a otras personas, hogares, comercios, y sanación kármica. Incluye audio de meditación para conectar con el símbolo individual de cada uno.</p>
-          <img src="https://source.unsplash.com/800x600/?akashic-records" alt="Clase de Registros Akashicos" class="w-full h-40 object-cover rounded mt-4" />
-          <iframe src="https://drive.google.com/file/d/13LcF7j0-BYj6mRQ0UqQxDN4dNpax6KVi/preview" width="640" height="480" allow="autoplay"></iframe>
+          <p class="text-gray-700">Nivel I: Lectura de Registros Akashicos, qué es el Akasha, cómo abrir nuestros
+            propios registros, práctica en vivo.<br>
+            Nivel II y III: Maestría en Lectura de Registros Akashicos, cómo abrir registros a otras personas, hogares,
+            comercios, y sanación kármica. Incluye audio de meditación para conectar con el símbolo individual de cada
+            uno.</p>
+          <img src="https://source.unsplash.com/800x600/?akashic-records" alt="Clase de Registros Akashicos"
+            class="w-full h-40 object-cover rounded mt-4" />
+          <iframe src="https://drive.google.com/file/d/13LcF7j0-BYj6mRQ0UqQxDN4dNpax6KVi/preview" width="640"
+            height="480" allow="autoplay"></iframe>
         </div>
-           <div class="bg-white p-6 rounded shadow-lg">
-          <img src="https://source.unsplash.com/800x600/?course" alt="Curso" class="w-full h-64 object-cover rounded mb-4" />
+        <div class="bg-white p-6 rounded shadow-lg">
+          <img src="https://source.unsplash.com/800x600/?course" alt="Curso"
+            class="w-full h-64 object-cover rounded mb-4" />
           <p class="text-lg mb-4 text-purple-700">
             <strong>Modalidad por Zoom:</strong> 4 Encuentros<br>
             <strong>Fechas:</strong> Jueves 10, 17, 24 y 31 de Octubre<br>
@@ -110,22 +106,21 @@
             <strong>Material:</strong> Se envía material en PDF<br>
             <strong>Valor Total:</strong> $24,900 (pesos argentinos) / U$d 30,00 (pago por Western Union)
           </p>
-          <a href="https://mpago.la/1JJUKba" class="bg-pink-400 text-white py-2 px-6 rounded hover:bg-pink-300" target="_blank">Link de Pago</a>
+          <a href="https://mpago.la/1JJUKba" class="bg-pink-400 text-white py-2 px-6 rounded hover:bg-pink-300"
+            target="_blank">Link de Pago</a>
           <p class="text-purple-700 mt-4">Transferencia a CBU 1430001713015184340017 BRUBANK<br>
-          Alias: cuerda.muelle.tiempo<br>
-          Cuevas Mariel Ermelinda</p>
-            <!-- Aviso de WhatsApp -->
+            Alias: cuerda.muelle.tiempo<br>
+            Cuevas Mariel Ermelinda</p>
+          <!-- Aviso de WhatsApp -->
           <div class="mt-8 bg-purple-50 p-4 rounded shadow-lg">
             <p class="text-lg text-purple-700 mb-4">
-              Una vez que hayas realizado el pago, por favor ingresa al grupo de WhatsApp para recibir más detalles sobre el curso.
+              Una vez que hayas realizado el pago, por favor ingresa al grupo de WhatsApp para recibir más detalles
+              sobre el curso.
             </p>
-           <!-- Botón de WhatsApp mejorado para responsive -->
-            <a
-              href="https://chat.whatsapp.com/IRCNKMPZyIqLR88S9tpfqy"
+            <!-- Botón de WhatsApp mejorado para responsive -->
+            <a href="https://chat.whatsapp.com/IRCNKMPZyIqLR88S9tpfqy"
               class="bg-green-500 text-white py-3 px-6 rounded-lg font-medium hover:bg-green-400 w-full md:w-auto block text-center"
-              target="_blank"
-              >Unirme al Grupo de WhatsApp</a
-            >
+              target="_blank">Unirme al Grupo de WhatsApp</a>
           </div>
 
         </div>
@@ -140,11 +135,8 @@
           Para más información o para inscribirte en nuestras clases, no dudes
           en contactarme.
         </p>
-        <a
-          href="wa.me/+5493544438308"
-          class="bg-green-400 text-white py-2 px-6 rounded hover:bg-pink-300"
-          >Enviar WhatsApp</a
-        >
+        <a href="wa.me/+5493544438308" class="bg-green-400 text-white py-2 px-6 rounded hover:bg-pink-300">Enviar
+          WhatsApp</a>
       </div>
     </section>
 
